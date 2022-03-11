07427ecfd6c1284f5bed4c52a6939991d4abed0c
HW <a.k_windsor@protonmail.com>
2022-03-11 20:08:21 +0100

    save plugin for seats | localStorage

--- script.js
+++ script.js
@@ -16,17 +16,30 @@ function selectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   const selectedCount = selectedSeats.length
   count.innerText = selectedCount;
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  console.log(seatsIndex)
+
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+}
+
+function load() {
+
 }
 // Calculate total price
 function selectedPrice() {
   return total.innerText = count.innerText * ticketPrice
 }
 
+
+
 // Movie select event
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
   selectedCount();
   selectedPrice();
+
 })
 
 // Seat click event
